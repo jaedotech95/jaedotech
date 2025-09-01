3f9ae00bde9ee6ec6fd9d5916ab312d4b4f12907
jaedotech95 <jaedotech95@gmail.com>
2025-09-01 14:39:10 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: '재도테크',
   description: '재도테크는 경량랙, 중량랙, 파렛트랙 등 산업용 랙의 판매·시공·철거 전문업체입니다. 창고, 물류센터, 공장 등 대형 공간은 물론 가정 및 소규모 매장 맞춤형 랙 설치 및 견적 문의 가능합니다.',
   generator: 'jaedotech',
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
